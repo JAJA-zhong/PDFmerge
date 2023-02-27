fddcf7d058bab51736c69c51f65fcb14a282f842
JAJADELL <jiajaja@icloud.com>
2023-02-27 13:16:26 +0800

    20230227

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ class PDFmerge:
 
     def run(self):
         # 获取指定目录下的所有PDF文件并组成列表
-        pdf_lst = [f for f in os.listdir(self.pdfiles) if f.endswith('.pdf')]
-        # 生成PDF文件目录地址并组成列表
-        pdfadd_lst = [os.path.join(target_path, filename) for filename in pdf_lst]
+        pdf_lst = [files for files in os.listdir(self.pdfiles) if files.endswith('.pdf')]
+        print(pdf_lst)
+        # 生成PDF文件目录地址并组成列表并去除水印文件
+        pdfadd_lst = [os.path.join(target_path, filename) for filename in pdf_lst if filename !="watermark.pdf"]
+        print(pdfadd_lst)
         # 实例化
         file_merger = PdfFileMerger(strict=False)
         # 初始化页数
@@ -37,17 +39,16 @@ class PDFmerge:
             time.sleep(0.1)
         # 写入合并文件
         file_merger.write(f'合并文件_{get_datetime()}.pdf')
+        print("PDF合并完成。")
         return f'合并文件_{get_datetime()}.pdf'
 
-    def add_watermark(self,pdf_file_in, pdf_file_mark, pdf_file_out):
+    def add_watermark(self,pdf_file_in, pdf_file_mark, pdf_file_out):#输入文件，水印文件，输出文件
         """把水印添加到pdf中"""
         pdf_output = PdfFileWriter()
         input_stream = open(pdf_file_in, 'rb')
         pdf_input = PdfFileReader(input_stream, strict=False)
-
         # 获取PDF文件的页数
         pageNum = pdf_input.getNumPages()
-
         # 读入水印pdf文件
         pdf_watermark = PdfFileReader(open(pdf_file_mark, 'rb'), strict=False)
         # 给每一页打水印
@@ -57,6 +58,7 @@ class PDFmerge:
             page.compressContentStreams()  # 压缩内容
             pdf_output.addPage(page)
         pdf_output.write(open(pdf_file_out, 'wb'))
+        print("水印合并完成。")
 
 
 if __name__ == "__main__":
@@ -66,11 +68,11 @@ if __name__ == "__main__":
         pass
     else:
         target_path = './'
-
     pdfmerger = PDFmerge(target_path)
     pdf_file_in=pdfmerger.run()
-    pdf_file_mark=r'watermark.pdf'
-    water_file=f"水印文件_{pdf_file_in[-18:]}"
+    pdf_file_mark=f'{target_path}watermark.pdf'
+    water_file=f"{target_path}水印文件_{pdf_file_in[-18:]}"
     water_path=input("是否添加水印(1)：")
     if water_path=='1':
-        pdfmerger.add_watermark(pdf_file_in,pdf_file_mark,water_file)
+        print(f"{target_path}{pdf_file_in}", pdf_file_mark, water_file)
+        pdfmerger.add_watermark(f"{target_path}{pdf_file_in}",pdf_file_mark,water_file)
